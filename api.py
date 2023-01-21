3e003e3f526e4a55b147fbab367dfdb94b5c7af6
peter <pstobiassen@gmail.com>
2023-01-21 13:57:47 -0500

    hooked up discover endpoint to discover devices and return their host

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 import flask
+from flask import jsonify, request
+from discover import DiscoverDevices
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -7,6 +9,11 @@ app.config["DEBUG"] = True
 @app.route("/", methods=["GET"])
 def home():
     return "<h1>My Kasa API</h1><p>This site is a prototype API for my Kasa Smart Device App.</p>"
+
+
+@app.route("/api/discover", methods=["GET"])
+def discover_devices():
+    return jsonify(DiscoverDevices())
 
 
 app.run()
--- discover.py
+++ discover.py
@@ -2,11 +2,25 @@ import asyncio
 import ipaddress
 from kasa import Discover
 
-for ip in ipaddress.IPv4Network("192.168.1.0/24"):
-    device = asyncio.run(Discover.discover(target=str(ip), timeout=0.3))
-    if device:
-        print(f"ip: {ip} device: {device}")
+devices = []
 
+
+def DiscoverDevices():
+    for ip in ipaddress.IPv4Network("192.168.1.0/24"):
+        asyncio.run(
+            Discover.discover(target=str(ip), timeout=0.5, on_discovered=print_alias)
+        )
+    return devices
+
+
+async def print_alias(dev):
+    devices.append({"host": dev.host, "alias": dev.alias})
+    print(f"Discovered {dev}")
+
+
+# my_devices = DiscoverDevices()
+
+# print(f"my devices: {my_devices}")
 
 # device = asyncio.run(Discover.discover(target="192.168.1.117"))
 # print(f"ip: 192.168.1.117 device: {device}")
